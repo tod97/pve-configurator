3086e68451ce8d063f4b01f2b55ff21cadfcd595
Francesco <todinofrancesco97@gmail.com>
2022-05-01 17:34:50 +0200

    _document config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Righteous"', 'sans-serif'],
+        body: ['"Righteous"', 'sans-serif'],
+      },
       transitionProperty: {
         width: 'width',
       },
